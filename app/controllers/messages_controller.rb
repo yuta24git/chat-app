0ffe2b444f709a8da3e0674f179191e0d544d4ca
yuta24git <whs.soccer.24.0803@gmail.com>
2023-07-20 16:02:51 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,17 @@
 class MessagesController < ApplicationController
   def index
-    @rooms = Room.all
+    @message = Message.new
+    @rooms = Room.find(params[:room_id])
+  end
+
+  def create
+    @room = Room.find(params[:room_id])
+    @message = @room.messages.new(message_params)
+    @message.save
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:message).merge(user_id: current_user.id)
   end
 end
